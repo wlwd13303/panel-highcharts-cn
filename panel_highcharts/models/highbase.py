c52ce5ec13614cee8b78b3776cf619eec9d1770f
markqiu <qiucheng@jinniuai.com>
2022-08-22 14:14:24 +0800

    1. 增加中文插件 2. 改为国内的网址

--- panel_highcharts/models/highbase.py
+++ panel_highcharts/models/highbase.py
@@ -5,43 +5,43 @@ from bokeh.core.properties import Any, Dict, Nullable, String
 from bokeh.models import LayoutDOM
 from panel.util import classproperty
 
-PATH_HIGH_CHARTS = "https://code.highcharts.com.cn/highcharts.js"
+PATH_HIGH_CHARTS = "https://cdn.highcharts.com.cn/highcharts.js"
 PATH_REQUIRE_HIGH_CHARTS = "https://code.highcharts.com.cn"
 
 PATHS = OrderedDict(
     [
-        ("highcharts-3d", "https://code.highcharts.com.cn/highcharts/highcharts-3d.js"),
-        ("highcharts-more", "https://code.highcharts.com.cn/highcharts/highcharts-more.js"),
-        ("highcharts/modules/stock", "https://code.highcharts.com.cn/modules/stock.js"),
-        ("highcharts/modules/map", "https://code.highcharts.com.cn/maps/modules/map.js"),
-        ("highcharts/modules/coloraxis", "https://code.highcharts.com.cn/maps/modules/coloraxis.js"),
+        ("highcharts-3d", "https://cdn.highcharts.com.cn/highcharts-3d.js"),
+        ("highcharts-more", "https://cdn.highcharts.com.cn/highcharts-more.js"),
+        ("highcharts/modules/stock", "https://cdn.highcharts.com.cn/modules/stock.js"),
+        ("highcharts/modules/map", "https://cdn.highcharts.com.cn/maps/modules/map.js"),
+        ("highcharts/modules/coloraxis", "https://cdn.highcharts.com.cn/maps/modules/coloraxis.js"),
         (
             "highcharts/modules/marker-clusters",
-            "https://code.highcharts.com.cn/maps/modules/marker-clusters.js",
+            "https://cdn.highcharts.com.cn/maps/modules/marker-clusters.js",
         ),
-        ("highcharts/modules/gantt", "https://code.highcharts.com.cn/modules/gantt.js"),
-        ("highcharts/modules/exporting", "https://code.highcharts.com.cn/modules/exporting.js"),
-        ("highcharts/modules/export-data", "https://code.highcharts.com.cn/modules/export-data.js"),
-        ("highcharts/modules/networkgraph", "https://code.highcharts.com.cn/modules/networkgraph.js"),
-        ("highcharts/modules/annotations", "https://code.highcharts.com.cn/modules/annotations.js"),
-        ("highcharts/modules/boost", "https://code.highcharts.com.cn/modules/boost.js"),
-        ("highcharts/modules/broken-axis", "https://code.highcharts.com.cn/modules/broken-axis.js"),
-        ("highcharts/modules/canvas-tools", "https://code.highcharts.com.cn/modules/canvas-tools.js"),
-        ("highcharts/modules/data", "https://code.highcharts.com.cn/modules/data.js"),
-        ("highcharts/modules/drilldown", "https://code.highcharts.com.cn/modules/drilldown.js"),
-        ("highcharts/modules/funnel", "https://code.highcharts.com.cn/modules/funnel.js"),
-        ("highcharts/modules/heatmap", "https://code.highcharts.com.cn/modules/heatmap.js"),
+        ("highcharts/modules/gantt", "https://cdn.highcharts.com.cn/modules/gantt.js"),
+        ("highcharts/modules/exporting", "https://cdn.highcharts.com.cn/modules/exporting.js"),
+        ("highcharts/modules/export-data", "https://cdn.highcharts.com.cn/modules/export-data.js"),
+        ("highcharts/modules/networkgraph", "https://cdn.highcharts.com.cn/modules/networkgraph.js"),
+        ("highcharts/modules/annotations", "https://cdn.highcharts.com.cn/modules/annotations.js"),
+        ("highcharts/modules/boost", "https://cdn.highcharts.com.cn/modules/boost.js"),
+        ("highcharts/modules/broken-axis", "https://cdn.highcharts.com.cn/modules/broken-axis.js"),
+        ("highcharts/modules/canvas-tools", "https://cdn.highcharts.com.cn/modules/canvas-tools.js"),
+        ("highcharts/modules/data", "https://cdn.highcharts.com.cn/modules/data.js"),
+        ("highcharts/modules/drilldown", "https://cdn.highcharts.com.cn/modules/drilldown.js"),
+        ("highcharts/modules/funnel", "https://cdn.highcharts.com.cn/modules/funnel.js"),
+        ("highcharts/modules/heatmap", "https://cdn.highcharts.com.cn/modules/heatmap.js"),
         (
             "highcharts/modules/no-data",
-            "https://code.highcharts.com.cn/modules/no-data-to-display.js",
+            "https://cdn.highcharts.com.cn/modules/no-data-to-display.js",
         ),
         (
             "highcharts/modules/offline-exporting",
-            "https://code.highcharts.com.cn/modules/offline-exporting.js",
+            "https://cdn.highcharts.com.cn/modules/offline-exporting.js",
         ),
-        ("highcharts/modules/solid-gauge", "https://code.highcharts.com.cn/modules/solid-gauge.js"),
-        ("highcharts/modules/treemap", "https://code.highcharts.com.cn/modules/treemap.js"),
-        ("highcharts/modules/variwide", "https://code.highcharts.com.cn/modules/variwide.js"),
+        ("highcharts/modules/solid-gauge", "https://cdn.highcharts.com.cn/modules/solid-gauge.js"),
+        ("highcharts/modules/treemap", "https://cdn.highcharts.com.cn/modules/treemap.js"),
+        ("highcharts/modules/variwide", "https://cdn.highcharts.com.cn/modules/variwide.js"),
     ]
 )
 
@@ -57,10 +57,10 @@ class HighBase(LayoutDOM):
     event = Nullable(Dict(String, Any))
 
     __javascript__ = [
-        "https://code.highcharts.com.cn/highcharts.js",
-        "https://code.highcharts.com.cn/modules/export-data.js",
-        "https://code.highcharts.com.cn/modules/exporting.js",
-        "https://code.highcharts.com.cn/highcharts-plugins/highcharts-zh_CN.js",
+        "https://cdn.highcharts.com.cn/highcharts.js",
+        "https://cdn.highcharts.com.cn/modules/export-data.js",
+        "https://cdn.highcharts.com.cn/modules/exporting.js",
+        "https://cdn.highcharts.com.cn/highcharts-plugins/highcharts-zh_CN.js",
     ]
 
     @classproperty
@@ -78,9 +78,9 @@ class HighBase(LayoutDOM):
             },
         ],
         "paths": {
-            "highcharts": "https://code.highcharts.com",
-            "highcharts/modules/exporting": "https://code.highcharts.com.cn/modules/exporting",
-            "highcharts/modules/export-data": "https://code.highcharts.com.cn/modules/export-data",
+            "highcharts": "https://cnd.highcharts.com.cn",
+            "highcharts/modules/exporting": "https://cdn.highcharts.com.cn/modules/exporting",
+            "highcharts/modules/export-data": "https://cdn.highcharts.com.cn/modules/export-data",
         },
         "exports": {
             "highcharts": "Highcharts",
